754e425b47936ecd4372fc5ed6951764366a5068
Valeria Gasik <valeria@selma.io>
2020-06-21 19:29:00 +0300

    Package updates

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello! Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <TitleBar>
+        <Avatar source={require("./assets/avatar.jpg")} />
+        <Title>Welcome back,</Title>
+        <Name>Meng</Name>
+      </TitleBar>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const Container = styled.View`
+  flex: 1;
+  background: #ffffff;
+  position: absolute;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 20px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
