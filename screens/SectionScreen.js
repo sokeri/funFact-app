a7acb5bbea39929de407af67597c1ed7ec0f88b8
Valeria Gasik <valeria@selma.io>
2020-07-02 18:20:12 +0300

    Styled card

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "react-native";
+import { TouchableOpacity, StatusBar } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 class SectionScreen extends React.Component {
@@ -8,28 +8,44 @@ class SectionScreen extends React.Component {
     headerShown: false,
   };
 
+  componenentDidMount() {
+    StatusBar.setBarStyle("light-content", true);
+  }
+
+  componentWillUnmount() {
+    StatusBar.setBarStyle("dark-content", true);
+  }
+
   render() {
     const { navigation } = this.props;
     const section = navigation.getParam("section");
 
     return (
       <Container>
+        <StatusBar hidden />
         <Cover>
           <Image source={section.image}></Image>
+          <Wrapper>
+            <Logo source={section.logo}></Logo>
+            <Subtitle>{section.subtitle}</Subtitle>
+          </Wrapper>
           <Title>{section.title}</Title>
           <Caption>{section.caption}</Caption>
-          <Logo source={section.logo}></Logo>
         </Cover>
-        <CloseView>
-          <Ionicons
-            name="ios-close"
-            size={44}
-            color="black"
-            onPress={() => {
-              this.props.navigation.goBack();
-            }}
-          />
-        </CloseView>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.goBack();
+          }}
+          style={{
+            position: "absolute",
+            top: 60,
+            right: 20,
+          }}
+        >
+          <CloseView>
+            <Ionicons name="ios-close" size={46} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
       </Container>
     );
   }
@@ -42,30 +58,43 @@ const Container = styled.View`
 `;
 
 const Cover = styled.View`
-  position: absolute;
-  width: 100%;
   height: 375px;
 `;
 
 const CloseView = styled.View`
-  width: 32px;
-  height: 32px;
-  background: white;
-  border-radius: 16px;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
-  align-items: center;
+  background-color: white;
+  border-radius: 40px;
+  width: 46px;
+  height: 46px;
   justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  position: absolute;
+  top: 40px;
+  left: 20px;
+  align-items: center;
 `;
 
 const Image = styled.Image`
-  position: absolute;
   width: 100%;
   height: 100%;
+  position: absolute;
 `;
 
 const Logo = styled.Image`
-  width: 32px;
-  height: 32px;
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
+`;
+
+const Subtitle = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 14px;
+  text-transform: uppercase;
 `;
 
 const Title = styled.Text`
@@ -74,15 +103,15 @@ const Title = styled.Text`
   font-weight: bold;
   width: 50%;
   position: absolute;
-  top: 70px;
+  top: 80px;
   left: 20px;
 `;
 
 const Caption = styled.Text`
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
   color: white;
   font-size: 17px;
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
   width: 50%;
 `;
